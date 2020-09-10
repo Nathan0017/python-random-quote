4bb25979883535cc387f59f11e41fc1637aaa2d6
Nathan <nathantotti17@gmail.com>
2020-09-10 12:47:36 +0530

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
+import random
 def Nathan():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  r=random.randint(0,13)
+  print(quotes[r])
 
 if __name__== "__main__":
   Nathan()
